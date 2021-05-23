93ae22cdeece88d498caa1c135a6fcf57f99c20a
Marek Niewiarowski <marek.niewiarowski@km.kongsberg.com>
2021-05-23 20:32:18 +0200

    Translation

--- SingleProjects/CountSeconds.py
+++ SingleProjects/CountSeconds.py
@@ -1,10 +1,10 @@
 try:
-    time = ( int( input( "Podaj ilość sekund: " ) ) )
+    time = ( int( input( "Input number of seconds: " ) ) )
 except ValueError:
-    print ("Podaj liczbę.")
+    print ("Is not a number.")
     quit()
 
 h = time // 3600
 m = (time % 3600 ) // 60
 s = time % 60
-print(f"{time}s = {h} godzin, {m} minut, {s} sekund")
+print(f"{time}s = {h} hours, {m} minutes, {s} seconds")
